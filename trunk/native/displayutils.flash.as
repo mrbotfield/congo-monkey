20a40bb1bd9b1328a412f50ca637c9b2c7bbba6b
goodreactions@gmail.com <goodreactions@gmail.com@a6ded688-a55d-c529-e4e4-c73276bdfc90>
2013-05-28 21:16:58 +0000

    initial checkin

---
+++ trunk/native/displayutils.flash.as
@@ -0,0 +1,17 @@
+
+// Native code for display utils.
+
+
+import flash.ui.Mouse
+
+function HideMouseCursor():void
+{
+	Mouse.hide();
+}
+
+function RestoreMouseCursor():void
+{
+	Mouse.show();
+}
+
+	
---
+++ trunk/native/displayutils.glfw.cpp
@@ -0,0 +1,14 @@
+
+// Native code for display utils.
+
+// See glfw user guide.
+
+static void HideMouseCursor()
+{
+	glfwDisable( GLFW_MOUSE_CURSOR );
+}
+
+static void RestoreMouseCursor()
+{
+	glfwEnable( GLFW_MOUSE_CURSOR );
+}
